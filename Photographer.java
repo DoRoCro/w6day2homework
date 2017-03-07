f8b4680c2e69664eda58e6717840c231516f488c
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-03-07 20:43:32 +0000

    test name() for Photographer

--- Photographer.java
+++ Photographer.java
@@ -1,6 +1,12 @@
-import java.util.HashMap;
+import java.util.*;
 
 public class Photographer {
+  public String name; 
+  private ArrayList<Camera> cameras;
 
+  public Photographer ( String name) {
+    this.name = name;
+  }
 
+  
 }
--- PhotographerTest.java
+++ PhotographerTest.java
@@ -2,6 +2,20 @@ import static org.junit.Assert.assertEquals;
 import org.junit.*;
 
 public class PhotographerTest {
+  Photographer photographer;
+  Camera camera;
 
+
+  @Test
+  public void hasName(){
+    photographer = new Photographer("David Bailey");
+    assertEquals("David Bailey", photographer.name);
+  } 
+
+ // @Test
+ // public void canAddCamera() {
+ //   photographer.addCamera(camera);
+ //   assertEquals(1, photographer.cameras.count() );
+ // }
 
 }
